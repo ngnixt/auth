e30dc34142bec055fb33c886d3d34cece5b986d0
lukaonik <lukaonik@gmail.com>
2022-03-15 16:22:41 +0400

    refactor: created permission,auth directives removed prefix

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthDirective } from './auth.directive';
+import { PermissionDirective } from './permission.directive';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [AuthDirective, PermissionDirective],
+  exports: [AuthDirective, PermissionDirective],
 })
 export class AuthModule {}
